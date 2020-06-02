27a78545ab48e107892fe3d66e64db6576c44679
Bill <william.hine2019@gmail.com>
2020-06-02 12:41:42 -0400

    Created Contestant class

---
+++ SweepStakes/Contestant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    public class Contestant
+    {
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
+    }
+}
